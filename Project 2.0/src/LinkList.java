0024c97d736c2f8bcd960f2b54faa26a68598bac
varunpoondi <varunmukundan@gmail.com>
2021-02-28 17:05:16 -0600

    First edit, finished up the project structure and able to output information. Only issue with sorting names.

--- Project 2.0/src/LinkList.java
+++ Project 2.0/src/LinkList.java
@@ -1,2 +1,93 @@
 public class LinkList {
+    private Node head;
+    
+    public void append(Node newNode){
+        if(head == null){
+           head = newNode;
+        }else{
+            Node currentNode = head;
+            while(currentNode.getNext() != null){
+                currentNode = currentNode.getNext();
+            }
+            currentNode.setNext(newNode);
+        }
+    }
+    private void prepend(Node newNode){
+       newNode.setNext(head);
+       head = newNode;
+    }
+    private void deleteCopyNodes(String playerName){
+        if(head != null){
+            Node node = head;
+            if(head.getPlayer().getName().equals(playerName)){
+                head.setNext(head.getNext());
+                node.setNext(null);
+            }else{
+                Node currentNode = head.getNext();
+                while(currentNode.getNext() != null && !currentNode.getNext().getPlayer().getName().equals(playerName)){
+                    currentNode = currentNode.getNext();
+                }
+                if(currentNode.getNext() != null){
+                    node = currentNode.getNext();
+                    currentNode.setNext(currentNode.getNext().getNext());
+                    node.setNext(null);
+                }
+            }
+        }
+    }
+    private void swap(Node currentNode, Node indexNode){
+        Player temp = currentNode.getPlayer();
+        currentNode.setPlayer(indexNode.getPlayer());
+        indexNode.setPlayer(temp);
+    }
+    private Node mergePlayer(Node currentNode, Node indexNode){
+        Node mergedNode = new Node(null);
+        mergedNode.getPlayer().setName(currentNode.getPlayer().getName());
+        mergedNode.getPlayer().getStats()[0] = currentNode.getPlayer().getStats()[0] + indexNode.getPlayer().getStats()[0];
+        mergedNode.getPlayer().getStats()[1] = currentNode.getPlayer().getStats()[1] + indexNode.getPlayer().getStats()[1];
+        mergedNode.getPlayer().getStats()[2] = currentNode.getPlayer().getStats()[2] + indexNode.getPlayer().getStats()[2];
+        mergedNode.getPlayer().getStats()[3] = currentNode.getPlayer().getStats()[3] + indexNode.getPlayer().getStats()[3];
+        mergedNode.getPlayer().getStats()[4] = currentNode.getPlayer().getStats()[4] + indexNode.getPlayer().getStats()[4];
+        mergedNode.getPlayer().getStats()[5] = currentNode.getPlayer().getStats()[5] + indexNode.getPlayer().getStats()[5];
+        mergedNode.getPlayer().getStats()[6] = currentNode.getPlayer().getStats()[6] + indexNode.getPlayer().getStats()[6];
+        mergedNode.getPlayer().getStats()[7] = currentNode.getPlayer().getStats()[7] + indexNode.getPlayer().getStats()[7];
+        
+        return mergedNode;
+    }
+    public void sortAlphabetically(){
+        Node currentNode = head;
+        Node indexNode = head.getNext();
+        if(head != null){
+            while(currentNode.getNext() != null){
+                while(indexNode.getNext() != null){
+                    int compare = currentNode.getPlayer().getName().compareTo(indexNode.getPlayer().getName());
+                    if(compare > 0){
+                        swap(currentNode, indexNode);
+                    }else if (compare == 0){
+                        Node newNode = mergePlayer(currentNode, indexNode);
+                        prepend(newNode);
+                        deleteCopyNodes(newNode.getPlayer().getName());
+                        sortAlphabetically();
+                    }
+                    indexNode = indexNode.getNext();
+                }
+                currentNode = currentNode.getNext();
+                print();
+                System.out.println();
+            }
+        }
+    }
+    public void print(){
+        printList(head);
+    }
+    public void printList(Node node){
+        if(node != null){
+            node.getPlayer().displayStats();
+            
+            printList(node.getNext());
+        }
+        
+    }
+
+    
 }
--- Project 2.0/src/Node.java
+++ Project 2.0/src/Node.java
@@ -1,2 +1,73 @@
 public class Node {
+   
+    private Node next;
+    private Player player;
+
+    public Node(Player player) {
+        this.next = null;
+        this.player = player;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
+class Player {
+    private String name;
+    private String battingRecord;
+    private final double[] stats = new double[8];
+
+    public Player(String name, String battingRecord) {
+        this.name = name;
+        this.battingRecord = battingRecord;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBattingRecord() {
+        return battingRecord;
+    }
+
+    public void setBattingRecord(String battingRecord) {
+        this.battingRecord = battingRecord;
+    }
+
+    public double[] getStats() {
+        return stats;
+    }
+
+    public void displayStats() {
+        System.out.print(getName() + "\t"
+                + Math.round(getStats()[0]) + "\t"
+                + Math.round(getStats()[1]) + "\t"
+                + Math.round(getStats()[2]) + "\t"
+                + Math.round(getStats()[3]) + "\t"
+                + Math.round(getStats()[4]) + "\t"
+                + Math.round(getStats()[5]) + "\t");
+        double BA = stats[6];
+        double OB = stats[7];
+
+        //convert to correct precision
+        String strBA = String.format("%.3f", (float) BA);
+        String strOB = String.format("%.3f", (float) OB);
+        System.out.print(strBA + "\t" + strOB + "\n");
+    }
 }
