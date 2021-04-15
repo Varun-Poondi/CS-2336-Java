394511a79153c28e559a66b777ed261ea898ae9f
varunpoondi <varunmukundan@gmail.com>
2021-04-15 18:43:33 -0500

    finished project structure

--- Project 4.0/src/Main.java
+++ Project 4.0/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.Scanner;
 
@@ -13,12 +15,22 @@ public class Main {
         String teamsFileName = fileChecker(scanner2);
         Scanner sc = new Scanner(new BufferedReader(new FileReader(keyFileName)));
         Scanner sc1 = new Scanner(new BufferedReader(new FileReader(teamsFileName)));
-        
+
         Hashtable<String, String> KeyFile = readKeyFile(sc);
-        Hashtable<String, Player> TeamsFile = readTeamsFile(sc1);
+        Hashtable<String, Player> TeamsTable = readTeamsFile(sc1, KeyFile);
+        
+        ArrayList<Player> Home = new ArrayList<>();
+        ArrayList<Player> Away = new ArrayList<>();
+        
+        separateMasterHashTable(TeamsTable, Home, Away);
+
+        printStats(Away, "AWAY");
+        printStats(Home, "HOME");
         
         
-        
+        //KeyFile.forEach((k, v) -> System.out.println("Key : " + k + ", Value : " + v));
+
+
     }
     public static String fileChecker(Scanner scanner){
         String fileName = scanner.next();
@@ -40,11 +52,60 @@ public class Main {
         return fileName;
     }
     public static Hashtable<String, String> readKeyFile(Scanner scanner){
-        
-     return null;   
+        Hashtable<String, String> table = new Hashtable<>();
+        String headerName = ""; //remove the hashTags later
+        String keyValue = "";
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            boolean isHeader = line.contains("#");
+            if(isHeader){
+                headerName =  line;
+                headerName = headerName.replace("#", "");
+                headerName = headerName.replace(" ", "");
+            }else if(!line.isEmpty()){
+                keyValue = line;
+                table.put(keyValue, headerName);
+            }
+        }
+     return table;   
     }
-    public static Hashtable<String, Player> readTeamsFile(Scanner scanner){
-        return null;
+    public static Hashtable<String, Player> readTeamsFile(Scanner scanner, Hashtable<String, String> keyTable){
+        Hashtable<String, Player> table = new Hashtable<>();
+        String name = "";
+        String code = "";
+        String team = "";
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            team = line.substring(0,1);
+            name = line.substring(2, line.lastIndexOf(" "));
+            code = line.substring(line.lastIndexOf(" ") + 1);
+            if(table.get(name) == null){
+                Player player = new Player(name, team);
+                player.decipherCode(code, keyTable);
+                table.put(name, player);
+            }else{
+                table.get(name).decipherCode(code, keyTable);
+            }
+        }
+        return table;
+    }
+    public static void separateMasterHashTable(Hashtable<String, Player> TeamsTable, ArrayList<Player> Home, ArrayList<Player> Away){
+        ArrayList<Player> master = new ArrayList<>(TeamsTable.values());
+        for (Player player : master) {
+            if (player.getTeam().equals("A")) {
+                Away.add(player);
+            } else {
+                Home.add(player);
+            }
+        }
+    }
+    public static void printStats(ArrayList<Player> team, String title){
+        System.out.println(title);
+        Collections.sort(team);
+        for (Player player : team) {
+            System.out.println(player);
+        }
+        System.out.println();
     }
     
     
--- Project 4.0/src/Player.java
+++ Project 4.0/src/Player.java
@@ -1,2 +1,94 @@
-public class Player {
+import java.util.Arrays;
+import java.util.Hashtable;
+
+public class Player implements Comparable<Player>{
+    private final String name;
+    private final String team;
+    private final double[] stats = new double[9];
+    private double plateApps;
+    public Player(String name, String team) {
+        this.name = name;
+        this.team = team;
+        plateApps = 0;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double[] getStats() {
+        return stats;
+    }
+    public void incrementStats(int index){
+        getStats()[index] += 1;
+    }
+
+    public void incrementPlateApps() {
+        this.plateApps += 1;
+    }
+
+    public void decipherCode(String code, Hashtable<String, String> keyTable){
+        String codeResult = keyTable.get(code);
+        switch (codeResult) {
+            case "HITS":
+                incrementStats(1);
+                incrementStats(0);
+                incrementPlateApps();
+                break;
+            case "STRIKEOUT":
+                incrementStats(3);
+                incrementStats(0);
+                incrementPlateApps();
+                break;
+            case "OUTS":
+                incrementStats(8);
+                incrementStats(0);
+                incrementPlateApps();
+                break;
+            case "WALK":
+                incrementStats(2);
+                incrementPlateApps();
+                break;
+            case "HITBYPITCH":
+                incrementStats(4);
+                incrementPlateApps();
+                break;
+            case "SACRIFICE":
+                incrementStats(5);
+                incrementPlateApps();
+                break;
+            case "ERRORS":
+                incrementPlateApps();
+                break;
+        }
+        double battingAverage = (stats[1])/(stats[0]);
+        double onBasePercentage = (stats[1] + stats[2] + stats[4])/(plateApps);
+
+        boolean baIsNan = Double.isNaN(battingAverage);
+        boolean obIsNan = Double.isNaN(onBasePercentage);
+        
+        if(baIsNan){
+            battingAverage = 0.000;
+        }
+        if(obIsNan){
+            onBasePercentage = 0.000;
+        }
+        stats[6] = battingAverage;
+        stats[7] = onBasePercentage;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\t" + Math.round(stats[0]) + "\t" + Math.round(stats[1]) + "\t" + Math.round(stats[2]) + "\t" + Math.round(stats[3]) + "\t" +
+                Math.round(stats[4]) + "\t" + Math.round(stats[5]) + "\t" + stats[6] + "\t" + stats[7]; 
+    }
+
+    @Override
+    public int compareTo(Player o) {
+        return this.getName().compareTo(o.getName());
+    }
 }
